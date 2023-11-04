3f5aafcdce112b4f11a2aa5ca1bf5a9853b524b7
Gayan <janith.gayan141@gmail.com>
2023-11-04 13:32:16 +0530

    Update Camera.js

--- assets/constants/Camera.js
+++ assets/constants/Camera.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import react, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -10,31 +10,24 @@ import {
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import axios from 'axios';
-import Modal from 'react-native-modal';
-
 import Loader from './Loader'
+import Modal from 'react-native-modal';
 
 const Camera = (props) => {
   const [isVisible, setIsVisible] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    setIsVisible(false);
-    setIsLoading(false);
-  }, []);
-
   const pickFromGallery = async () => {
     setIsVisible(false);
-    const { status } = await MediaLibrary.requestPermissionsAsync();
-    if (status === 'granted') {
-      // Gallery permissions granted, proceed with image selection
+    const { granted } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (granted) {
       let data = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
         aspect: [1, 1],
-        quality: 1,
+        quality: 1 // 1 means high quality
       });
-      if (!data.cancelled) {
+      if (!data.canceled) {
         let newFile = {
           uri: data.uri,
           type: `test/${data.uri.split('.')[1]}`,
@@ -49,15 +42,14 @@ const Camera = (props) => {
 
   const pickFromCamera = async () => {
     setIsVisible(false);
-    const { status } = await Camera.requestPermissionsAsync();
-    if (status === 'granted') {
-    // Camera permissions granted, proceed with image capture
-    let data = await ImagePicker.launchCameraAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
+    const { granted } = await Permissions.askAsync(Permissions.CAMERA);
+    if (granted) {
+      let data = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 1 
+      });
       if (!data.canceled) {
         let newFile = {
           uri: data.uri,
@@ -97,73 +89,80 @@ const Camera = (props) => {
     setIsVisible(true);
   };
 
-  return (
-    <View style={styles.rect5Stack}>
-      <Loader isLoading={isLoading} />
-      <TouchableOpacity
-        style={styles.rect5}
-        onPress={() => onShowModal()}
-      >
-      </TouchableOpacity>
-      <Image
-        source={require('../images/photography.png')}
-        resizeMode='contain'
-        style={styles.image8}
-      ></Image>
+
+    return (
+      <View style={styles.rect5Stack}>
+        <Loader isLoading={isLoading} />
+        <TouchableOpacity
+          style={styles.rect5}
+          onPress={() => {
+            // this.props.navigation.navigate('PredictionScreen');
+            onShowModal();
+          }}
+        >
+          <Text style={styles.takeAPicture}>Take a Picture</Text>
+        </TouchableOpacity>
+
+        <Image
+          source={require('../../assets/images/photography.png')}
+          resizeMode='contain'
+          style={styles.image8}
+        ></Image>
       <Modal
         isVisible={isVisible}
         onBackdropPress={() => setIsVisible(false)}
       >
-        <View
-          style={{
-            flex: 1,
-            alignItems: 'center',
-            justifyContent: 'center',
-            alignContent: 'center'
-          }}
-        >
-          <View style={styles.modalView}>
-            <Text style={styles.modalHeader}>Choose</Text>
-            <View style={styles.modalBody}>
-              <TouchableOpacity onPress={pickFromCamera}>
-                <Image
-                  source={require('../images/photo.png')}
-                  resizeMode='contain'
-                  style={styles.modalImage1}
-                ></Image>
-              </TouchableOpacity>
-              <TouchableOpacity onPress={pickFromGallery}>
-                <Image
-                  source={require('../images/memories.png')}
-                  resizeMode='contain'
-                  style={styles.modalImage2}
-                ></Image>
+          <View
+            style={{
+              flex: 1,
+              alignItems: 'center',
+              justifyContent: 'center',
+              alignContent: 'center'
+            }}
+          >
+            <View style={styles.modalView}>
+              <Text style={styles.modalHeader}>Choose</Text>
+              <View style={styles.modalBody}>
+                <TouchableOpacity onPress={pickFromCamera}>
+                  <Image
+                    source={require('../../assets/images/photo.png')}
+                    resizeMode='contain'
+                    style={styles.modalImage1}
+                  ></Image>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={pickFromGallery}>
+                  <Image
+                    source={require('../../assets/images/memories.png')}
+                    resizeMode='contain'
+                    style={styles.modalImage2}
+                  ></Image>
+                </TouchableOpacity>
+              </View>
+              <View style={styles.cameraRow}>
+                <Text style={styles.camera}>Camera</Text>
+                <Text style={styles.gallery}>Gallery</Text>
+              </View>
+              <TouchableOpacity
+                onPress={() => this.setState({ isVisible: false })}
+              >
+                <Text style={styles.modalCancel}>Cancel</Text>
               </TouchableOpacity>
             </View>
-            <View style={styles.cameraRow}>
-              <Text style={styles.camera}>Camera</Text>
-              <Text style={styles.gallery}>Gallery</Text>
-            </View>
-            <TouchableOpacity
-              onPress={() => setIsVisible(false)}
-            >
-              <Text style={styles.modalCancel}>Cancel</Text>
-            </TouchableOpacity>
           </View>
-        </View>
-      </Modal>
-    </View>
-  );
-};
+        </Modal>
+      </View>
+    );
+}
 
 const styles = StyleSheet.create({
   rect5Stack: {
     width: 201,
     height: 44,
-    marginTop: 50,
- },
+    marginTop: 40,
+    marginLeft: 19
+  },
   rect5: {
-    top: 1,
+    top: 8 ,
     left: 0,
     width: 201,
     height: 43,
@@ -178,16 +177,18 @@ const styles = StyleSheet.create({
     marginLeft: 84
   },
   image8: {
-    top: 0,
+    top: 8,
+    left: 25,
     width: 35,
     height: 42,
-    alignSelf:'center'
+    position: 'absolute'
   },
   modalView: {
     width: 239,
     height: 157,
     backgroundColor: 'white',
     borderRadius: 17,
+
     alignSelf: 'center'
   },
   modalHeader: {
@@ -238,5 +239,4 @@ const styles = StyleSheet.create({
     marginLeft: 180
   }
 });
-
 export default Camera;
